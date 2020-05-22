25510c6c65ef2ec6bf8cc813bb7a32febe7be905
Santiago Rodriguez <s.rodriguez20@uniandes.edu.co>
2020-05-21 19:13:04 -0500

    Case 3 code added

--- src/views/Performance/Case3.vue
+++ src/views/Performance/Case3.vue
@@ -11,8 +11,8 @@
           En esta actividad podemos realizar la configuración de parámetros de
           la aplicación en general. Un patrón anómalo en esta actividad se debe
           a la renderización del slider, en caso de que desplacemos la lista de
-          elementos al inicio o al final de manera inmediata, el consumo de memoria
-          aumentará hasta que la aplicación falle. 
+          elementos al inicio o al final de manera inmediata, el consumo de
+          memoria aumentará hasta que la aplicación falle.
         </p>
       </card>
     </div>
@@ -26,10 +26,10 @@
         </div>
         <p>
           El porcentaje de consumo de CPU en esta actividad es casi nulo
-          ubicándose alrededor de un 5%. En el momento en el cual desplazamos 
-          la lista de elementos al inicio y al final de manera inmediata, el 
-          consumo de CPU aumenta considerablemente hasta situarse en 15%. 
-          La mayor carga es procesada por el Thread project.android
+          ubicándose alrededor de un 5%. En el momento en el cual desplazamos la
+          lista de elementos al inicio y al final de manera inmediata, el
+          consumo de CPU aumenta considerablemente hasta situarse en 15%. La
+          mayor carga es procesada por el Thread project.android
         </p>
         <img src="img/performance/cpu3.png" alt="cpu1" />
       </card>
@@ -79,12 +79,66 @@
           </div>
         </div>
         <p>
-          Esta sección presenta un bajo consumo de energía en general. 
-          El consumo aumenta de manera considerable en el momento en 
-          cual desplazamos inmediatamente hacia el inicio y hacia el final 
-          la lista de configuraciones
+          Esta sección presenta un bajo consumo de energía en general. El
+          consumo aumenta de manera considerable en el momento en cual
+          desplazamos inmediatamente hacia el inicio y hacia el final la lista
+          de configuraciones
         </p>
         <img src="img/performance/energy3.png" alt="cpu1" />
+      </card>
+    </div>
+    <div class="col-12 col-md-8 offset-md-2 mt-5">
+      <card class="content">
+        <div slot="header" class="row align-items-center">
+          <div class="col">
+            <h1>
+              Codigo
+            </h1>
+          </div>
+        </div>
+        <p>
+          Buscamos la implementación de esta sección de configuraciones para
+          entender un poco más y encontramos que esta sección está implementada
+          en su totalidad utilizando algunas clases del paquete and
+          android.preferences. Esto les permite declara en un archivo xml con el
+          listado de preferencias y asignarlo directamente a la clase
+          SettingsPreferences que hereda de PreferenceActivity (clase
+          perteneciente al paquete android.preferences).
+        </p>
+        <figure>
+          <img src="img/code/prefs.png" alt="prefs snippet" />
+
+          <figcaption>
+            Archivo con listado de preferencias agrupadas por categoría.
+            Fragmento de código obtenido del siguiente
+            <a
+              href="https://github.com/guardianproject/orbot/blob/master/app/src/main/res/xml/preferences.xml"
+            >
+              archivo
+            </a>
+          </figcaption>
+        </figure>
+        <p>
+          Esta implementación se sale de las manos de los desarrolladores ya que
+          depende de la implementación de android, sin embargo encontramos que
+          actualmente todos las clases pertenecientes a android.preferences se
+          encuentran deprecadas en este momento. En su lugar, se recomienda
+          utilizar la librería Preferences de AndroidX la cual asegura tener un
+          comportamiento consistente en la mayoría de dispositivos.
+        </p>
+        <figure>
+          <img src="img/code/settings.png" alt="settings snippet" />
+
+          <figcaption>
+            Clase heredada de PreferenceActivity. Fragmento de código obtenido
+            del siguiente
+            <a
+              href="https://github.com/guardianproject/orbot/blob/master/app/src/main/java/org/torproject/android/settings/SettingsPreferences.java"
+            >
+              archivo
+            </a>
+          </figcaption>
+        </figure>
       </card>
     </div>
   </div>
